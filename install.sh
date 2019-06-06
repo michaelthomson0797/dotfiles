86a13c5a3d9618c517dbf50b55a12f5070715bed
Michael Thomson <michaelthomson0797@gmail.com>
2019-06-06 18:58:54 -0400

    first draft of installation script

---
+++ install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+################################################################################
+### Globals
+################################################################################
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ARCH_DEPENDENCIES=
+DEBIAN_DEPENDENCIES=
+DEPENDENCIES=(zsh bash emacs vim git)
+
+################################################################################
+### GUI and Shell
+################################################################################
+
+# Install GUI?
+read -p "Set up GUI? (y/n) " yesorno
+case $yesorno in
+    y)
+        GUI=1
+        ;;
+    *)
+        GUI=0
+        ;;
+esac
+
+if [ $GUI -eq 1 ]; then
+    DEPENDENCIES+=(i3-gaps)
+fi
+
+# Installing dependencies
+echo "Installing ${DEPENDENCIES[*]}"
+if cat /etc/os-release | grep -qE "(Ubuntu|Debian)"; then
+    sudo apt-get install ${DEPENDENCIES[*]} ${DEBIAN_DEPENDENCIES[*]}
+elif cat /etc/os-release | grep -qE "Arch"; then
+    sudo pacman -S ${DEPENDENCIES[*]} ${ARCH_DEPENDENCIES[*]}
+else
+    echo "Cannot determin distro"
+fi
+    
+# Check if zsh is default
+if [[ "$(getent passwd $USER | cut -d: -f7)" = *"zsh"* ]]
+then
+    echo "zsh is already the default shell"
+else
+    if command -v zsh > /dev/null 2>&1
+    then
+        read -p "Make zsh the default shell? (y/n) " yesorno
+        case $yesorno in
+            y)
+                ZSH_PATH=`which zsh`
+                echo "setting zsh as default shell"
+                chsh $USER -s $ZSH_PATH
+                ;;
+            *)
+                echo "zsh will not be set to default"
+                ;;
+        esac
+    else
+        echo "zsh is not installed. skipping chsh"
+    fi
+fi
+
+
+################################################################################
+### Dotfiles
+################################################################################
+
+# source zsh config
+read -p "set up shell? (y/n) " yesorno
+case $yesorno in
+    y)
+        touch $HOME/.zshrc
+        echo "source $DIR/zshrc" >> "$HOME/.zshrc"
+        ;;
+    *) echo "skipping shell setup"
+esac
+
+# link emacs config
+if [ -e "$HOME/.emacs.d" ]
+then
+    echo ".emacs.d already exists. skipping..."
+else
+    read -p "set up $HOME/.emacs.d? (y/n) " yesorno
+    case $yesorno in
+        y) ln -ds "$DIR/emacs.d" "$HOME/.emacs.d" ;;
+        *) echo "skipping $HOME/.emacs.d"
+    esac
+fi
+
+# link vimrc config
+if [ -e "$HOME/.vimrc" ]
+then
+    echo ".vimrc already exists. skipping..."
+else
+    read -p "set up $HOME/.vimrc? (y/n) " yesorno
+    case $yesorno in
+        y) ln -ds "$DIR/vimrc" "$HOME/.vimrc" ;;
+        *) echo "skipping $HOME/.vimrc"
+    esac
+fi
