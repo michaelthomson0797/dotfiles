26f36d247d502a32874716c532ff8dd6cd441063
Michael Thomson <michaelthomson0797@gmail.com>
2019-06-06 13:03:25 -0400

    changed dotfiles

---
+++ .emacs.d/elpa/websocket-20190408.152/websocket-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "websocket" "20190408.152" "Emacs WebSocket client and server" '((cl-lib "0.5")) :commit "ef1e504fecca9321213ea837bff53df1e39f2262" :keywords '("communication" "websocket" "server") :authors '(("Andrew Hyatt" . "ahyatt@gmail.com")) :maintainer '("Andrew Hyatt" . "ahyatt@gmail.com"))
