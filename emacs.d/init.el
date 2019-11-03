3226cec213cf79e502fbe4afbf85e55717de672f
Michael Thomson <michaelthomson0797@gmail.com>
2019-11-02 21:51:37 -0400

    cleaned up and added intero haskell support

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,4 +1,11 @@
 ;; load user-specific variables
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (when (file-exists-p "~/.emacs.d/user.el")
   (load "~/.emacs.d/user.el"))
 
